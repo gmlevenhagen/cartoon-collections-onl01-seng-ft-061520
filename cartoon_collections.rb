e2bcfcee091825566c54b1c688ef8809ea2effe0
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 13:44:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
     return cheese_types[i] if array.include(cheese_types[i])
+  end
     i += 1
   end 
 end
