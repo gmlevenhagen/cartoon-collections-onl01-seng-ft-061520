63e89db11d81319f880bd74c3897040dbf9d3602
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-24 13:18:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  i = 0
+  while i < array.length
+    return array[i] if yield(array[i])
+    i = i + 1
+  end# Your code here
 end
 
 def summon_captain_planet# code an argument here
